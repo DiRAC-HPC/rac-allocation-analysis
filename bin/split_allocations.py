63a7260ea9436cbb734277c48818c4e6a90e1e1e
Andy Turner <a.turner@epcc.ed.ac.uk>
2019-10-11 13:54:23 +0100

    Updated spltting allocations

--- bin/split_allocations.py
+++ bin/split_allocations.py
@@ -13,19 +13,21 @@ quarters_pool = cycle(quarters)
 
 # Read in the total allocations
 infile = sys.argv[1].strip()
-total_df = pd.read_csv(infile, quotechar="'", quoting=2)
+types = {'ProjectID': str, 'RACID': str, 'RACID': str, 'Start Year': np.int32, 'Start Quarter': str, 'Quarters': np.int32, 'Resource': str, 'Allocation': np.float64}
+total_df = pd.read_csv(infile, quotechar="'", quoting=2, dtype=types)
 
 print(total_df)
 
 # Write out the quarterly allocations
+stem = sys.argv[1].strip().split('.')[0]
+outfile = stem + '_split.csv'
+csvfile = open(outfile, 'w')
 
-csvfile = open("rac1.csv", 'w')
-
-csvfile.write("'ProjectID','RACID','Quarter','Year','Resource','Allocation'\n")
+csvfile.write("'ProjectID','RACID','PI','Quarter','Year','Resource','Allocation'\n")
 for index, row in total_df.iterrows():
     year = row['Start Year'] + 0.25
     for i in range(int(row['Quarters'])):
-        csvfile.write("'{:}','{:}','{:}',{:d},{:},{:.3f}\n".format(row["ProjectID"], row["RACID"], next(quarters_pool), int(year), row['Resource'], float(row['Allocation'])/row['Quarters']))
+        csvfile.write("'{:}','{:}','{:}','{:}',{:d},'{:}',{:.3f}\n".format(row["ProjectID"], row["RACID"], row["PI"], next(quarters_pool), int(year), row['Resource'], float(row['Allocation'])/row['Quarters']))
         year = year + 0.25
 
 csvfile.close()
